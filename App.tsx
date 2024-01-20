05d22a43ba88ee1e61c08f85703751b36825fe24
Dipankar Giri <109781560+giridipankar@users.noreply.github.com>
2024-01-20 11:38:19 +0000

    Pushed root project

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Login from './components/Login/Login.tsx';
+
+export default function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
